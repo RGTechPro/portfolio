4726111ad26191b8dc8a3610c5f28c654b5ce0c9
Rishabh Gupta <73791635+RGTechPro@users.noreply.github.com>
2023-02-02 23:47:39 +0530

    added custom clippers

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -27,8 +27,8 @@ class HomePage extends StatelessWidget {
                     ),
                     Text(
                       'Gupta',
-                      style:
-                          TextStyle(fontFamily: 'Neue', color: Color(0xff04c189)),
+                      style: TextStyle(
+                          fontFamily: 'Neue', color: Color(0xff04c189)),
                     ),
                     Text(
                       '.',
@@ -38,12 +38,14 @@ class HomePage extends StatelessWidget {
                 ),
               ),
               actions: [
-                SizedBox(height: 70,
+                SizedBox(
+                  height: 70,
                   child: Padding(
                     padding: const EdgeInsets.only(right: 40, top: 30),
                     child: Row(
                       children: [
-                        SizedBox(height: 50,
+                        SizedBox(
+                          height: 50,
                           child: TextButton(
                               onPressed: () {},
                               child: Text(
@@ -78,9 +80,62 @@ class HomePage extends StatelessWidget {
               backgroundColor: Colors.transparent,
             ),
           ),
+          body: Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 150, top: 50),
+                  child: ClipPath(
+                    clipper: CustomClipperImage(),
+                    child: Container(
+                      height: 450,
+                      child: Image.asset(
+                        'images/my_photo.png',
+                        //height: 100,
+                        alignment: Alignment.topLeft,
+                        width: 350,
+                        fit: BoxFit.fitWidth,
+                      ),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.rectangle, color: Color(0xff252734)),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
           backgroundColor: Colors.transparent,
         ),
       ),
     );
+  }
+}
+
+class CustomClipperImage extends CustomClipper<Path> {
+  @override
+  getClip(Size size) {
+    var path = Path();
+    path.lineTo(250.0, 0);
+    path.lineTo(250, 100);
+    path.lineTo(360, 100);
+    path.lineTo(360, 330);
+    path.lineTo(275, 330);
+    path.lineTo(275, 400);
+    path.lineTo(0, 400);
+    path.lineTo(0, 300);
+    path.lineTo(85, 300);
+    path.lineTo(85, 270);
+    path.lineTo(70, 270);
+    path.lineTo(70, 210);
+    path.lineTo(0, 210);
+    path.lineTo(0, 0);
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return true;
   }
 }
